f424fddebd3c50d800315db74f3b234f85a6ae7b
Brian Embry <b.a.embry@gmail.com>
2019-07-28 12:37:22 -0500

    Seed questions and answers tables; implement model for questions and questions route

---
+++ db/seed/seedAnswers.js
@@ -0,0 +1,65 @@
+secrets = require('./config.secrets');
+const faker = require('faker');
+const mysql = require('mysql');
+const Promise = require('bluebird');
+
+var db = mysql.createConnection({
+  host: 'localhost',
+  user: 'brian',
+  password: secrets.password,
+});
+
+Promise.promisifyAll(db);
+
+db.connect(function(err) {
+  if (err) console.error(err);
+  console.log('Connected!');
+});
+
+const random = function(n) {
+  return Math.floor(Math.random() * (n + 1));
+};
+
+const buildQuery = function(begin, end) {
+  if (end >= 200000) {
+    return;
+  }
+  const answer = function(id) {
+    return `(${id},"${faker.random.words()}", "${faker.name.firstName()}", ${random(
+      20
+    )}, ${random(20)})`;
+  };
+  let query = `INSERT INTO answers (question_id, text, author, helpful, unhelpful) VALUES ${answer(
+    1
+  )}`;
+
+  for (let i = begin; i < end; i++) {
+    if (i % 10000 === 0) {
+      console.log(i);
+    }
+    query += `,${answer(i)}`;
+  }
+  query += ';';
+  return query;
+};
+
+const seedAnswers = function(begin, end) {
+  db.query('USE lowes', async err => {
+    if (err) {
+      console.error(err);
+    }
+    let start = Date.now();
+    const query = buildQuery(begin, end);
+    console.log('Query built ');
+    db.query(query, err => {
+      if (err) {
+        throw err;
+      } else {
+        console.info('DB seeded in ' + (Date.now() - start) + ' ms');
+        seedAnswers(begin + 50000, end + 50000);
+      }
+    });
+  });
+};
+
+seedAnswers(1, 50000);
---
+++ db/seed/seedQuestions.js
@@ -0,0 +1,63 @@
+secrets = require('./config.secrets');
+const faker = require('faker');
+const mysql = require('mysql');
+const Promise = require('bluebird');
+
+var db = mysql.createConnection({
+  host: 'localhost',
+  user: 'brian',
+  password: secrets.password,
+});
+
+Promise.promisifyAll(db);
+
+db.connect(function(err) {
+  if (err) console.error(err);
+  console.log('Connected!');
+});
+
+const random = function(n) {
+  return Math.floor(Math.random() * (n + 1));
+};
+
+const buildQuery = function(begin, end) {
+  if (end > 210000) {
+    return;
+  }
+  const question = function(id) {
+    return `(${id},"${faker.random.words()}", "${faker.name.firstName()}")`;
+  };
+  let query = `INSERT INTO questions (question_id, question, author) VALUES ${question(
+    1
+  )}`;
+
+  for (let i = begin; i < end; i++) {
+    if (i % 10000 === 0) {
+      console.log(i);
+    }
+    query += `,${question(i)}`;
+  }
+  query += ';';
+  return query;
+};
+
+const seedQuestions = function(begin, end) {
+  db.query('USE lowes', async err => {
+    if (err) {
+      console.error(err);
+    }
+    let start = Date.now();
+    const query = buildQuery(begin, end);
+    console.log('Query built ');
+    db.query(query, err => {
+      if (err) {
+        throw err;
+      } else {
+        console.info('DB seeded in ' + (Date.now() - start) + ' ms');
+        seedQuestions(begin + 50000, end + 50000);
+      }
+    });
+  });
+};
+
+seedQuestions(1, 50000);
