24d466085c7b81014c68917a85628f139ef40251
Brian Embry <b.a.embry@gmail.com>
2019-07-29 17:14:33 -0500

    Update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,12 +7,12 @@ module.exports = {
   ],
   deploy: {
     production: {
-      user: 'linux',
-      host: 'ec2-3-17-37-8.us-east-2.compute.amazonaws.com',
-      key: '~/.ssh/lowes.pem',
+      user: 'ubuntu',
+      host: 'ec2-18-216-43-170.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/lowes2.pem',
       ref: 'origin/master',
       repo: 'https://github.com/baembry/lowes-product-description-2.0.git',
-      path: '/home',
+      path: '/home/ubuntu/lowesProductDescription',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js',
     },
   },
